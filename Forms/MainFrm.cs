71e165ac374a6206a4818f9d4dae10108091139e
MelQ <MelQQ29@yandex.ru>
2022-07-06 18:39:26 +0200

    Auth with db log/pas verif
    Created .cs Class with connection to DB
    Login/Password verification from DB

--- Forms/MainFrm.cs
+++ Forms/MainFrm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TrainTravelApp
 {
@@ -22,7 +23,23 @@ namespace TrainTravelApp
             String loginEmployee = loginTB.Text; // переменная которая берет данные Логина из текст бокса
             String passEmployee = passTB.Text; // переменная которая берет данные Пароля из текст бокса
 
-            //DB
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM refEmployee WHERE Login = @uL AND Password = @uP", db.getConnection());
+            command.Parameters.Add("@uL", SqlDbType.VarChar).Value = loginEmployee;
+            command.Parameters.Add("@uP", SqlDbType.VarChar).Value = passEmployee;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                MessageBox.Show("Успешная авторизация!");
+            else
+                MessageBox.Show("Введен неверный логин или пароль.");
         }
     }
 }
